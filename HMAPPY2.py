a0db56f4fb5967f95051922f4a545bbf1d9c3c43
joydeep1701 <joydeep1701@gmail.com>
2019-02-06 16:42:51 +0530

    06-02

--- HMAPPY2.py
+++ HMAPPY2.py
@@ -1,5 +1,22 @@
+def gcd(a, b):
+    if a == 0:
+        return b
+    return gcd(b % a, a)
+
+def lcm(a,b):
+    return (a*b)/gcd(a,b)
+
 T = int(input())
 while T:
     N, A, B, K = [int(x) for x in input().strip().split(" ")]
     
+    a_s = N // A
+    c_s = N // B
+    b_s = N // lcm(A, B)
+
+    if (a_s - 2*b_s + c_s) >= K:
+        print("Win") 
+    else:
+        print("Lose")
+
     T = T - 1
